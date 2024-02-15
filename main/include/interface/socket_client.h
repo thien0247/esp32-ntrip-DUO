753b8ff4840bae726c4350fe17f3d77e92c5bf6a
incarvr6 <49254419+incarvr6@users.noreply.github.com>
2024-02-15 00:38:42 +0000

    Delete main directory

--- main/include/interface/socket_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESP32_XBEE_SOCKET_CLIENT_H
-#define ESP32_XBEE_SOCKET_CLIENT_H
-
-#include <stdint.h>
-#include <esp_event_base.h>
-
-void socket_client_init();
-
-#endif //ESP32_XBEE_SOCKET_CLIENT_H
--- main/include/web_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef ESP32_XBEE_WEB_SERVER_H
-#define ESP32_XBEE_WEB_SERVER_H
-
-void web_server_init();
-
-#endif //ESP32_XBEE_WEB_SERVER_H
